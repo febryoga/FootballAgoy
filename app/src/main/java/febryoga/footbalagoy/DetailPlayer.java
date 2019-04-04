536ecc435b3b60c42c5fb1860d7b241422482799
febryoga <febryogasaputra@gmail.com>
2019-04-04 21:43:47 +0700

    beta

---
+++ app/src/main/java/febryoga/footbalagoy/DetailPlayer.java
@@ -0,0 +1,33 @@
+package febryoga.footbalagoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailPlayer extends AppCompatActivity {
+
+    public static String EXTRA_PLAYER = "extra_player";
+    TextView tvNo, tvNama;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_player);
+
+        tvNo = findViewById(R.id.tvNo);
+        tvNama = findViewById(R.id.tvNama);
+
+
+        PlayerItem playerItem =  getIntent().getParcelableExtra(EXTRA_PLAYER);
+
+        String no = playerItem.getNo();
+        String name = playerItem.getName();
+
+        tvNo.setText(no);
+        tvNama.setText(name);
+
+
+    }
+}
